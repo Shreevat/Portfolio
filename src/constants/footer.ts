73373721d73de32dd19bbfcaa825891c2247b466
shreevat <shree.kuro@gmail.com>
2024-12-02 16:54:55 +0545

    Updates

--- src/constants/footer.ts
+++
@@ -1,7 +0,0 @@
-export const FOOTER_NAVIATION_LINKS = [
-    { name: "Services", href: "#services" },
-    { name: "Career", href: "#career" },
-    { name: "Contact", href: "/Contact" },
-  ];
-
-  export const FOOTER_CONTACT = "9871386666";
