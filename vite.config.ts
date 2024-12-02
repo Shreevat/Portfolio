b52cd06292aa4cb5c68f829c23e7859eab73f883
shreevat <shree.kuro@gmail.com>
2024-12-02 16:50:59 +0545

    multi page on github build

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/", // Ensure this base path for GitHub Pages
+  base: "/Portfolio/",  // Ensure this base path for GitHub Pages
   build: {
     rollupOptions: {
-      input: {
-        main: "./index.html",  // Main entry point
-        contact: "./src/Pages/Contact/index.html",  // Additional page for /Contact
-        projects: "./src/Pages/Projects/index.html", // Additional page for /Projects
-      },
+      input: "./index.html",  // Only the main entry point for Vite
     },
   },
 });
