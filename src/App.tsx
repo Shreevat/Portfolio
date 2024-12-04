4ca607b9fd176de630c77a4720ac04881b44cc83
shreevat <shree.kuro@gmail.com>
2024-12-04 13:35:21 +0545

    deploy build 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useContext } from 'react';
-import { DarkModeContext } from './context/darkModeContext';
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 import ScrollToTop from "./components/UI/ScrollToTop";
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
 import Projects from "./Pages/Projects";
-import { DarkModeProvider } from './context/darkModeContext';
+import { DarkModeProvider } from "./context/darkModeContext";
 
 function AppContent() {
   const { darkMode } = useContext(DarkModeContext);
 
   return (
-    <Router basename="/Portfolio">
-      <div className={`bg-background ${darkMode ? 'dark-mode' : ''}`}>
+    <Router basename="/">
+      <div className={`bg-background ${darkMode ? "dark-mode" : ""}`}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Contact" element={<Contact />} />
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', 
+  base: "./",
   build: {
-    outDir: "dist",  // Specify the build output directory
+    outDir: "dist", // Specify the build output directory
     rollupOptions: {
-      input: "./index.html",  // The main entry point for Vite
+      input: "./index.html", // The main entry point for Vite
     },
   },
 });
