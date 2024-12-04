99e2bbbf6e60eca0f18f8523a02a35beba75d5af
shreevat <shree.kuro@gmail.com>
2024-12-04 13:52:56 +0545

    deploy build with fixed routes

--- src/constants/header.ts
+++ src/constants/header.ts
@@ -3,6 +3,6 @@ export const HEADER_NAME = "PORTFOLIO";
 export const HEADER_NAVIATION_LINKS = [
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skill" },
-  { name: "Contact Me", href: "/Portfolio/Contact" },
-  { name: "Projects", href: "/Portfolio/Projects" },
+  { name: "Contact Me", href: "/Contact" },
+  { name: "Projects", href: "/Projects" },
 ];
